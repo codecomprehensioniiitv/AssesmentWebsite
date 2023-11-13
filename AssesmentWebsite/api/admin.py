7702eccef16cebaf28fb4e30b0a20dafa4896175
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2023-11-13 17:25:43 +0530

    Adding data to admin

--- AssesmentWebsite/api/admin.py
+++ AssesmentWebsite/api/admin.py
@@ -1,3 +1,10 @@
+# api/admin.py
 from django.contrib import admin
+from .models import Demographic, Expertise, Code , Evaluation, Score, Time , QuestionBank, QuestionsBankLevel, Question # Import all your models
 
-# Register your models here.
+# Create a list of models
+models_to_register = [Demographic, Expertise, Code, Evaluation, Score, Time, QuestionBank, QuestionsBankLevel, Question ]# Import all your models
+
+# Register all models in the admin
+for model in models_to_register:
+    admin.site.register(model)
