1aabd9e157fc674144d4c4a1a6dec354a96b8cfc
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2023-11-15 18:09:20 +0530

    Adding Javascript as A language option

--- AssesmentWebsite/api/Demographic/models.py
+++ AssesmentWebsite/api/Demographic/models.py
@@ -1,33 +1,14 @@
 from django.db import models
 
-gender_choices =(
-    ("1", "Female"),
-    ("2", "Male"),
-    ("3", "NaN")
-)
+gender_choices = (("1", "Female"), ("2", "Male"), ("3", "NaN"))
 
-profession_choices =(
-    ("1", "Student"),
-    ("2", "Industrialist"),
-    ("3", "Professor")
-)
+profession_choices = (("1", "Student"), ("2", "Industrialist"), ("3", "Professor"))
 
-language_choices =(
-    ("1", "Python"),
-    ("2", "C++"),
-    ("3", "Java")
-)
+language_choices = (("1", "Python"), ("2", "C++"), ("3", "Javascript"))
 
-level_choices =(
-    ("1", "Beginner"),
-    ("2", "Intermediate"),
-    ("3", "Expert")
-)
-duration_choices =(
-    ("1", "<1 year"),
-    ("2", "1-3 years"),
-    ("3", ">3 years")
-)   
+level_choices = (("1", "Beginner"), ("2", "Intermediate"), ("3", "Expert"))
+duration_choices = (("1", "<1 year"), ("2", "1-3 years"), ("3", ">3 years"))
+
 
 # Demographic : User profile
 class Demographic(models.Model):
@@ -40,6 +21,7 @@ class Demographic(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     profession = models.CharField(choices=profession_choices, max_length=100)
+
     def __str__(self):
         return f"{self.uid}"
 
@@ -47,9 +29,10 @@ class Demographic(models.Model):
 # Expertise model for getting the programming language.
 class Expertise(models.Model):
     eid = models.AutoField(primary_key=True)
-    fuid = models.ForeignKey(Demographic,related_name='demographic_id', on_delete=models.CASCADE, null= True)
+    fuid = models.ForeignKey(
+        Demographic, related_name="demographic_id", on_delete=models.CASCADE, null=True
+    )
     programming_language = models.CharField(choices=language_choices, max_length=100)
     level = models.CharField(choices=level_choices, max_length=100)
     duration = models.CharField(choices=duration_choices, max_length=100)
     time = models.IntegerField()
-    
--- AssesmentWebsite/api/Questions/models.py
+++ AssesmentWebsite/api/Questions/models.py
@@ -2,52 +2,47 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.forms import CharField
 
-level_choices =(
-    ("1", "Beginner"),
-    ("2", "Intermediate"),
-    ("3", "Expert")
-)
-language_choices =(
-    ("1", "Python"),
-    ("2", "C++"),
-    ("3", "Java")
-)
-
+level_choices = (("1", "Beginner"), ("2", "Intermediate"), ("3", "Expert"))
+language_choices = (("1", "Python"), ("2", "C++"), ("3", "JavaScript"))
 
 
 class QuestionBank(models.Model):
     qbid = models.AutoField(primary_key=True)
     # aid = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
-    admin_programming_language = models.CharField(choices=language_choices, max_length=100)
+    admin_programming_language = models.CharField(
+        choices=language_choices, max_length=100
+    )
     # level = models.CharField(choices=level_choices, max_length=100)
 
 
 class QuestionsBankLevel(models.Model):
     qblid = models.AutoField(primary_key=True)
-    fqbid = models.ForeignKey(QuestionBank,on_delete=models.CASCADE, null= True)
+    fqbid = models.ForeignKey(QuestionBank, on_delete=models.CASCADE, null=True)
     qlevel = models.CharField(choices=level_choices, max_length=100)
 
 
 class Code(models.Model):
     cid = models.AutoField(primary_key=True)
-    fqblid = models.ForeignKey(QuestionsBankLevel,on_delete=models.CASCADE, null= True)
+    fqblid = models.ForeignKey(QuestionsBankLevel, on_delete=models.CASCADE, null=True)
     code_image = models.ImageField()
     code_text = models.CharField(max_length=500, null=True)
     code_time = models.FloatField(null=True)
     # code_read_time = models.IntegerField(null=True)
     question_time = models.FloatField(null=True)
+
     # question_read_time = models.IntegerField(null=True)
     @property
     def imageURL(self):
         try:
             url = self.image.url
         except:
-            url = ''
+            url = ""
         return url
-    
+
+
 class Question(models.Model):
     qid = models.AutoField(primary_key=True)
-    fcid = models.ForeignKey(Code, on_delete=models.CASCADE, null= True)
+    fcid = models.ForeignKey(Code, on_delete=models.CASCADE, null=True)
     question_text = models.CharField(max_length=500, null=True)
     option1 = models.CharField(max_length=500, null=True)
     option2 = models.CharField(max_length=500, null=True)
@@ -55,4 +50,4 @@ class Question(models.Model):
     option4 = models.CharField(max_length=500, null=True)
     correct_option = models.CharField(max_length=500, null=True)
     marks = models.FloatField(null=True)
-    # question_time = models.IntegerField(null=True)
+    question_time = models.IntegerField(null=True)
