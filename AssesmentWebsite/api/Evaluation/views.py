974e7914e740f0a64616f627dd25ba9db43c8b6b
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2023-11-19 01:45:33 +0530

    Adding Functions In one View

--- AssesmentWebsite/api/Evaluation/views.py
+++ AssesmentWebsite/api/Evaluation/views.py
@@ -1,5 +1,5 @@
-from .models import *
-from .serializers import *
+from .Evaluation.models import *
+from .Evaluation.serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
