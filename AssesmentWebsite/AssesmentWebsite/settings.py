571191b5c7ef534761aba3db4e8a7afb3095b48f
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2023-11-25 16:40:41 +0530

    ADDING postgres SETUP

--- AssesmentWebsite/AssesmentWebsite/settings.py
+++ AssesmentWebsite/AssesmentWebsite/settings.py
@@ -77,15 +77,14 @@ WSGI_APPLICATION = "AssesmentWebsite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgres://admin:unP7jlZNqyRMfQR8U8aOwp8dJvOKB0sb@dpg-clgsrnb1hq4c73bkb420-a.oregon-postgres.render.com/db_5ckd",
+        conn_max_age=600,
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
